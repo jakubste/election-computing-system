07964cd42aebd6109dc0d0a7ce93da25e9dffd67
Jakub Stępak <jakub.ste@gmail.com>
2016-04-03 00:39:56 +0200

    Add base version of ell_p calculation

--- brute/ecs/vote.py
+++ brute/ecs/vote.py
@@ -8,3 +8,20 @@ class Vote(object):
         self.repeats = repeats
         self.preference = preference
         super(Vote, self).__init__()
+
+    @staticmethod
+    def ell_p_norm(p, values):
+        values = map(lambda x: x**p, values)
+        value = reduce(lambda x, y: x+y, values)
+        try:
+            return pow(value, 1.0/p)
+            # pow() implementation casts value to float format
+            # This means if we want calculate root of number
+            # that is bigger than float max we need to do it
+            # different way
+        except OverflowError:
+            return -1
+            pass
+
+    def calculate_committee_score(self, committee, p):
+        pass
