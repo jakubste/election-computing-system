5851e609528fc8e7d8c7b9a4b46fbce00458a98c
Tomasz Kasprzyk <kasptom@gmail.com>
2016-04-05 00:59:15 +0200

    removed "generate_voters_candidates_pair" function

--- brute/votemaker/population.py
+++ brute/votemaker/population.py
@@ -7,14 +7,9 @@ class Population:
     def __init__(self, voters_number, candidates_number):
         self.voters_number = voters_number
         self.candidates_number = candidates_number
-        self.voters_coordinates = []
-        self.candidates_coordinates = []
-
-    def generate_voters_candidates_pair(self):
         self.voters_coordinates = self.load_coordinates(self.voters_number)
         self.candidates_coordinates = self.load_coordinates(self.candidates_number)
-        return self.voters_coordinates, self.candidates_coordinates
 
     @staticmethod
     def load_coordinates(size):
-        return [(gauss(config.RANGE), gauss(config.RANGE)) for i in range(1, size)]
+        return [(gauss(config.MEAN, config.SIGMA), gauss(config.MEAN, config.SIGMA)) for i in range(1, size)]
