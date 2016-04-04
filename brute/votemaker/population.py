a8055798866a3dbe1438e99db7bd370ad0180073
Tomasz Kasprzyk <kasptom@gmail.com>
2016-04-05 00:36:53 +0200

    refactoring; corrected square size mistake

--- brute/votemaker/population.py
+++ brute/votemaker/population.py
@@ -1,22 +1,20 @@
-import random as r
+from random import gauss
+import config
 
 
 class Population:
 
     def __init__(self, voters_number, candidates_number):
-        self.v = voters_number
-        self.c = candidates_number
-        self.v_coord = []
-        self.c_coord = []
+        self.voters_number = voters_number
+        self.candidates_number = candidates_number
+        self.voters_coordinates = []
+        self.candidates_coordinates = []
 
-    def generate_voters(self):
-        self.v_coord = [[r.uniform(1, 1000)
-                       for i in range(1, self.v)]
-                       for j in range(1, self.v)]
+    def generate_voters_candidates_pair(self):
+        self.voters_coordinates = self.load_coordinates(self.voters_number)
+        self.candidates_coordinates = self.load_coordinates(self.candidates_number)
+        return self.voters_coordinates, self.candidates_coordinates
 
-    def generate_candidates(self):
-        self.c_coord = [[r.uniform(1, 1000)
-                       for i in range(1, self.c)]
-                       for j in range(1, self.c)]
-
-
+    @staticmethod
+    def load_coordinates(size):
+        return [(gauss(config.RANGE), gauss(config.RANGE)) for i in range(1, size)]
