7e0a834f9e37e6d30e14e14b841d76e3f67d30b6
Tomasz Kasprzyk <kasptom@gmail.com>
2016-04-15 11:11:21 +0200

    added initial preferences generation

---
+++ brute/votemaker/preferences.py
@@ -0,0 +1,27 @@
+from math import sqrt
+
+
+class Preferences:
+    def __init__(self, population):
+        """
+            :type population: votemaker.population.Population
+        """
+        self.voters_coordinates = population.voters_coordinates
+        self.candidates_coordinates = population.candidates_coordinates
+        self.voters_preferences = self.compute_preferences()
+
+    def compute_preferences(self):
+        """
+            For each voter arranges candidates in order of euclidean norm distances
+            :rtype: dictionary, key: tuple of double, value: list of tuples of double
+        """
+        voters_preferences = {}
+        for voter in self.voters_coordinates:
+            voter_preferences = self.candidates_coordinates.sort(
+                key=lambda candidate: self.compute_euclidean_norm(voter, candidate))
+            voters_preferences[voter] = voter_preferences
+        return voters_preferences
+
+    @staticmethod
+    def compute_euclidean_norm((x_1, y_1), (x_2, y_2)):
+        return sqrt((x_1 - x_2) ** 2 + (y_1 - y_2) ** 2)
