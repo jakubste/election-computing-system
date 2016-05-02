66be6060304f8ee3041d08a08a614ff9ec6ff70e
Jakub Stępak <jakub.ste@gmail.com>
2016-05-02 21:40:56 +0200

    Voters and candidates models

--- ecs/elections/models.py
+++ ecs/elections/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from ecs.elections.geo.models import Point
 
 
 class Election(models.Model):
@@ -13,3 +13,25 @@ class Election(models.Model):
 
     def __unicode__(self):
         return self.name
+
+
+class Candidate(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    position = models.ForeignKey(Point, null=True)
+    preferences = models.ManyToManyField('Voter', through='Preference')
+    election = models.ForeignKey(Election, related_name='candidates')
+
+    def __unicode__(self):
+        return self.name
+
+
+class Voter(models.Model):
+    repeats = models.IntegerField(default=1)
+    position = models.ForeignKey(Point, null=True)
+    election = models.ForeignKey(Election, related_name='voters')
+
+
+class Preference(models.Model):
+    candidate = models.ForeignKey(Candidate)
+    voter = models.ForeignKey(Voter, related_name='preferences')
+    preference = models.IntegerField(null=False)
