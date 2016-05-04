1c1658b67aa6f820f20d80628dbe9acdfd498475
Jakub Stępak <jakub.ste@gmail.com>
2016-05-04 21:02:31 +0200

    Add tests to accounts app
    Also tidy test folder in election app.

--- ecs/elections/tests/test_views.py
+++ ecs/elections/tests/test_views.py
@@ -70,7 +70,7 @@ class ElectionLoadDataFromFileTestCase(TestCase):
         # basic test for data loading
         filename = 'test_data1.txt'
         filename = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), filename)
+            os.path.dirname(os.path.abspath(__file__)), 'input_data', filename)
         request = RequestFactory().get(self.get_url())
         view = ElectionLoadDataFormView(request=request, election=self.election)
         view.load_data_from_file(open(filename))
@@ -84,7 +84,7 @@ class ElectionLoadDataFromFileTestCase(TestCase):
     def test_load_data_from_file_by_form(self):
         filename = 'test_data1.txt'
         filename = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), filename)
+            os.path.dirname(os.path.abspath(__file__)), 'input_data', filename)
         response = self.client.post(self.get_url(), {
             'file': open(filename)
         })
@@ -96,90 +96,90 @@ class ElectionLoadDataFromFileTestCase(TestCase):
     def test_incorrect_type_of_candidates_number_exception(self):
         filename = 'test_exceptions1.soc'
         filename = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), filename)
+            os.path.dirname(os.path.abspath(__file__)), 'input_data', filename)
         view = ElectionLoadDataFormView(election=ElectionFactory.create())
         self.assertRaises(IncorrectTypeOfCandidatesNumberException, view.load_data_from_file, open(filename))
 
     def test_candidates_name_incorrect_format_exception(self):
         filename = 'test_exceptions2.soc'
         filename = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), filename)
+            os.path.dirname(os.path.abspath(__file__)), 'input_data', filename)
         view = ElectionLoadDataFormView(election=ElectionFactory.create())
         self.assertRaises(CandidatesNameIncorrectFormatException, view.load_data_from_file, open(filename))
 
     def test_summing_line_format_exception(self):
         filename = 'test_exceptions3.soc'
         filename = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), filename)
+            os.path.dirname(os.path.abspath(__file__)), 'input_data', filename)
         view = ElectionLoadDataFormView(election=ElectionFactory.create())
         self.assertRaises(SummingLineFormatException, view.load_data_from_file, open(filename))
 
     def test_summing_line_type_exception(self):
         filename = 'test_exceptions4.soc'
         filename = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), filename)
+            os.path.dirname(os.path.abspath(__file__)), 'input_data', filename)
         view = ElectionLoadDataFormView(election=ElectionFactory.create())
         self.assertRaises(SummingLineTypeException, view.load_data_from_file, open(filename))
 
     def test_bad_data_format_exception(self):
         filename = 'test_exceptions5.soc'
         filename = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), filename)
+            os.path.dirname(os.path.abspath(__file__)), 'input_data', filename)
         view = ElectionLoadDataFormView(election=ElectionFactory.create())
         self.assertRaises(BadDataFormatException, view.load_data_from_file, open(filename))
 
     def test_preference_order_type_exception(self):
         filename = 'test_exceptions6.soc'
         filename = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), filename)
+            os.path.dirname(os.path.abspath(__file__)), 'input_data', filename)
         view = ElectionLoadDataFormView(election=ElectionFactory.create())
         self.assertRaises(PreferenceOrderTypeException, view.load_data_from_file, open(filename))
 
     def test_preference_order_logic_exception(self):
         filename = 'test_exceptions7.soc'
         filename = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), filename)
+            os.path.dirname(os.path.abspath(__file__)), 'input_data', filename)
         view = ElectionLoadDataFormView(election=ElectionFactory.create())
         self.assertRaises(PreferenceOrderLogicException, view.load_data_from_file, open(filename))
 
     def test_non_positive_number_of_votes_exception(self):
         filename = 'test_exceptions8.soc'
         filename = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), filename)
+            os.path.dirname(os.path.abspath(__file__)), 'input_data', filename)
         view = ElectionLoadDataFormView(election=ElectionFactory.create())
         self.assertRaises(NonPositiveNumberOfVotesException, view.load_data_from_file, open(filename))
 
     def test_incorrect_votes_number_unique_votes_relation_exception(self):
         filename = 'test_exceptions9.soc'
         filename = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), filename)
+            os.path.dirname(os.path.abspath(__file__)), 'input_data', filename)
         view = ElectionLoadDataFormView(election=ElectionFactory.create())
         self.assertRaises(IncorrectVotesNumberUniqueVotesRelationException, view.load_data_from_file, open(filename))
 
     def test_preference_order_length_exception(self):
         filename = 'test_exceptions10.soc'
         filename = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), filename)
+            os.path.dirname(os.path.abspath(__file__)), 'input_data', filename)
         view = ElectionLoadDataFormView(election=ElectionFactory.create())
         self.assertRaises(PreferenceOrderLengthException, view.load_data_from_file, open(filename))
 
     def test_preference_order_beyond_scope_exception(self):
         filename = 'test_exceptions11.soc'
         filename = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), filename)
+            os.path.dirname(os.path.abspath(__file__)), 'input_data', filename)
         view = ElectionLoadDataFormView(election=ElectionFactory.create())
         self.assertRaises(PreferenceOrderBeyondScopeException, view.load_data_from_file, open(filename))
 
     def test_incorrect_preference_order_exception(self):
         filename = 'test_exceptions12.soc'
         filename = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), filename)
+            os.path.dirname(os.path.abspath(__file__)), 'input_data', filename)
         view = ElectionLoadDataFormView(election=ElectionFactory.create())
         self.assertRaises(IncorrectPreferenceOrderException, view.load_data_from_file, open(filename))
 
     def test_number_of_votes_inconsistency_exception(self):
         filename = 'test_exceptions13.soc'
         filename = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), filename)
+            os.path.dirname(os.path.abspath(__file__)), 'input_data', filename)
         view = ElectionLoadDataFormView(election=ElectionFactory.create())
         self.assertRaises(NumberOfVotesInconsistencyException, view.load_data_from_file, open(filename))
