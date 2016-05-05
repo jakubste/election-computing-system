78d2250ed0b7ab6fd3f88437e57295df85fb5d7b
Tomasz Kasprzyk <kasptom@gmail.com>
2016-05-06 00:40:27 +0200

    added test for is_generated flag

--- ecs/elections/tests/test_models.py
+++ ecs/elections/tests/test_models.py
@@ -1,4 +1,7 @@
-from ecs.elections.factories import ElectionFactory, VoterFactory, CandidateFactory
+from django.core.urlresolvers import reverse
+
+from ecs.elections.factories import ElectionFactory, VoterFactory, CandidateFactory, PointCandidateFactory, \
+    PointVoterFactory
 from ecs.utils.unittestcases import TestCase
 
 
@@ -10,3 +13,16 @@ class ElectionTestCase(TestCase):
         self.assertEqual(election.is_set_up(), False)
         CandidateFactory.create(election=election)
         self.assertEqual(election.is_set_up(), True)
+
+    def test_is_set_generated_flag(self):
+        file_election = ElectionFactory.create()
+        file_cs = CandidateFactory.create_batch(4, election=file_election)
+        file_vs = VoterFactory.create_batch(10, election=file_election)
+        file_url = reverse('elections:chart_data', args=(file_election.pk,))
+
+        gauss_election = ElectionFactory.create()
+        gauss_cs = PointCandidateFactory.create_batch(4, election=gauss_election)
+        gauss_vs = PointVoterFactory.create_batch(10, election=gauss_election)
+        gauss_url = reverse('elections:chart_data', args=(gauss_election.pk,))
+        self.assertEqual(file_election.is_generated(), False)
+        self.assertEqual(gauss_election.is_set_up(), True)
