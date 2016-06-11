351efea2c59e7584cfccae02581da8ef3e0e5018
DanOgiela <dan.ogiela@gmail.com>
2016-06-11 23:12:41 +0200

    greedy algorithm implementation

---
+++ ecs/elections/algorithms/greedy_algorithm.py
@@ -0,0 +1,85 @@
+from ecs.elections.algorithms.algorithm import Algorithm
+from ecs.utils.math import ell_p_norm
+
+
+class GreedyAlgorithm(Algorithm):
+    preferences = None
+
+    def fetch_preferences(self):
+        self.preferences = {}
+        for voter in self.election.voters.all().prefetch_related('preferences__candidate'):
+            self.preferences[voter.pk] = {}
+            for preference in voter.preferences.all():
+                self.preferences[voter.pk].update({
+                    preference.candidate.pk: preference.preference
+                })
+
+    def run(self, p_parameter):
+
+        voters = self.election.voters.all()
+        candidates_number = self.election.candidates.count()
+
+        self.fetch_preferences()
+        candidates_still_fighting = self.election.candidates.all()
+        winning_committee = []
+        actual_voters_satisfaction = {}
+
+        for v in voters:
+            actual_voters_satisfaction[v.pk] = 0
+
+        for i in range(self.election.committee_size):
+            satisfaction_with_leading_candidate = 0
+            leading_candidate = None
+            for c in candidates_still_fighting:
+                satisfaction_with_given_candidate = 0
+                for v in voters:
+                    x = self.number_of_points_in_preference_order(c, v, candidates_number)
+                    satisfaction_of_given_voter = self.get_actual_satisfaction_of_given_voter(v,
+                                                                                              actual_voters_satisfaction)
+
+                    values = [satisfaction_of_given_voter, x]
+                    satisfaction_of_given_voter_with_given_candidate = ell_p_norm(values, p_parameter)
+
+                    satisfaction_with_given_candidate += satisfaction_of_given_voter_with_given_candidate
+
+                if satisfaction_with_given_candidate > satisfaction_with_leading_candidate:
+                    leading_candidate = c
+                    satisfaction_with_leading_candidate = satisfaction_with_given_candidate
+            self.update_voters_satisfaction(leading_candidate, voters, actual_voters_satisfaction, candidates_number,
+                                            p_parameter)
+            winning_committee.append(leading_candidate)
+            candidates_still_fighting = candidates_still_fighting.exclude(pk=leading_candidate.pk)
+
+        return tuple(winning_committee)
+
+    def number_of_points_in_preference_order(self, c, v, candidates_number):
+        """
+
+        :param v: Voter, c: Candidate, candidates_number: int
+        :return: int
+        """
+
+        return candidates_number - self.preferences[v.pk][c.pk]
+
+    def get_actual_satisfaction_of_given_voter(self, v, actual_voters_satisfaction):
+        """
+
+        :param v: Voter
+        :return: int
+        """
+        return actual_voters_satisfaction[v]
+
+    def update_voters_satisfaction(self, leading_candidate, voters, actual_voters_satisfaction, candidates_number, p):
+        """
+
+        :param leading_candidate: Candidate
+        :param voters: QuerySet
+        :param actual_voters_satisfaction: dict{Voter: int}
+        :param candidates_number: int
+        :param p: int
+        :return:
+        """
+        for v in voters:
+            x = self.number_of_points_in_preference_order(leading_candidate, v, candidates_number)
+            values = [actual_voters_satisfaction[v.pk], x]
+            actual_voters_satisfaction[v.pk] = ell_p_norm(values, p)
