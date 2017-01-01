cd0b3e14ef3e3d0dea792a68ad9e7e16f42b1f7c
Tomasz Kasprzyk <kasptom@users.noreply.github.com>
2017-01-01 21:46:13 +0100

    Algorithm comparison charts (#40)
    * Remove ScatterChart plugin
          * Use Chart js built in scatter chart
          * Add points_radii to enable "point" radius setting
          * Remove unused dependencies from javascripts.html
          * Adjust colors and radius of chart points
    
    * Fix Travis issue
    
    * Update tests
    
    * Add algorithms' comparison chart
    
          * Give ScatterChartView some more generic name
           * Add "Algorithms comparison" section to election_details.html
           * Create AlgorithmsChart view and add url to the data it fetches
           * Add linear chart creation in election.js
           * Give chart tests some more generic names
    
    * Fix error when no result for algorithm is given
    
    * Display chart data similar to p parameter slider
    
    * Make spanGaps option working
    
          * Update chartjs to 2.4.0
    
    * Squash x axis in algorithm charts
          * Remove unnecessary attribute from chart configuration
    
    * Add algorithms comparison chart test
    
    * Fix error with wrong value of p parameter on chart
    
    * Add titles to charts
    
          * Display time(p_parameter) chart for .soc generated elections
          * Update unit tests
    
    * Fix bug connected with chart  switching on hover
    
    * Keep algorithms chart monotonicity
    
          * Update tests
    
    * Fix chart update on hover bug, again...

--- ecs/utils/chart_views.py
+++ ecs/utils/chart_views.py
@@ -2,11 +2,13 @@ from django.http import JsonResponse
 from django.views.generic import View
 
 
-class ScatterChartMixin(View):
+class ChartMixin(View):
     datasets_number = 0
     labels = None
     colors = None
     points_stroke_colors = None
+    points_radii = None
+    title = ""
 
     def get_data(self):
         raise NotImplementedError
@@ -29,6 +31,15 @@ class ScatterChartMixin(View):
         else:
             return self.points_stroke_colors
 
+    def get_points_radii(self):
+        if not self.points_radii:
+            return [5 for i in xrange(self.datasets_number)]
+        else:
+            return self.points_radii
+
+    def get_title(self):
+        return self.title
+
     def get_datasets(self, *args, **kwargs):
         """
         Format data to Scatter dataset format
@@ -36,17 +47,22 @@ class ScatterChartMixin(View):
         labels = self.get_labels()
         colors = self.get_colors()
         point_stroke_colors = self.get_points_stroke_colors()
+        points_radii = self.get_points_radii()
         data = self.get_data()
 
-        return [
-            {
-                'label': labels[i],
-                'pointColor': colors[i],
-                'pointStrokeColor': point_stroke_colors[i],
-                'data': data[i]
-            }
-            for i in xrange(self.datasets_number)
-            ]
+        return {
+            'datasets': [
+                {
+                    'label': labels[i],
+                    'borderColor': colors[i],
+                    'backgroundColor': point_stroke_colors[i],
+                    'data': data[i],
+                    'pointRadius': points_radii[i]
+                }
+                for i in xrange(self.datasets_number)
+                ],
+            'title': self.get_title()
+        }
 
     def get(self, *args, **kwargs):
         return JsonResponse(
